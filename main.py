6812229a118114c6518a28acfaca98d101890409
KIM SEONG EUN <127196534+smilechild7@users.noreply.github.com>
2024-01-16 10:27:17 +0900

    code challange!

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-kse = {
-    "name" : "kse",
-    "XP" : 1000,
-    "team" : "Learning Crew"
-}
+class Player:
+    def __init__(self,name,team):
+        self.name = name
+        self.team = team
+        self.xp = 1500
 
-def introduce_player(player):
-    name = player["name"]
-    team = player["team"]
-    print(f"Hi my name is {name} and I play for {team} ")
+    def introduce(self):
+        print(f"Hi! I'm {self.name} and I play for {self.team}!")
 
-introduce_player(kse)
+class Team:
+    def __init__(self, team_name):
+        self.name = team_name
+        self.players = []
+    
+    def show_players(self):
+        for player in self.players:
+            player.introduce()
+
+    def add_player(self, name):
+        new_player = Player(name, self.name)
+        self.players.append(new_player)
+
+    def delete_player(self, name):
+        for player in self.players:
+            if player.name == name:
+                self.players.remove(player)
+
+    def show_xp_sum(self):
+        sum=0
+        for player in self.players:
+            sum+= player.xp
+        print(f"{self.name} have {sum} xp!")
+
+team_X = Team("team X")
+team_X.add_player("kse")
+
+team_Y = Team("team Y")
+team_Y.add_player("jjg")
+team_Y.add_player("gjh")
+team_Y.add_player("csm")
+team_Y.delete_player("csm")
+
+team_Y.show_players()
+
+team_Y.show_xp_sum()
